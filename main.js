5f8205b10f8473532fd85d020503c3e019640007
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-17 15:47:46 +0200

    assegnato variabile emailList

---
+++ main.js
@@ -0,0 +1,3 @@
+document.addEventListener("click", function() {
+    const emailList = document.getElementById("email-list");/* ottengo l'elemento con ID email-list dal DOM e lo assegno alla variabile emailList */
+});
